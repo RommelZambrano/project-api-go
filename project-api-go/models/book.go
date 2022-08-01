3ad478434ed64b394f2b9caf59237beb5e68c7fb
Rommel Nicolás Zambrano Gaona <84996374+RommelZambrano@users.noreply.github.com>
2022-07-31 23:29:46 -0500

    deploy

---
+++ project-api-go/models/book.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type Book struct {
+	Name   string  `json:"name" `
+	Gender string  `json:"gender" `
+	Author  string  `json:"author" `
+	Price  float32 `json:"price" `
+}
+
+type Response struct {
+	Status  int        `json:"status"`
+	Message string     `json:"message"`
+	Data    *fiber.Map `json:"data"`
+}
